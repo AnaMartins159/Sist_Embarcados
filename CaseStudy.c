8ba1cbb37b0348495362755f7082298f337fbbbd
leonardo-usp <leonardodvc@usp.br>
2022-06-16 10:52:23 -0300

    Repairs case study

--- CaseStudy.c
+++ CaseStudy.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 int main() {
-    char str[30] = "1234567890 This is a test";
+    char str[30] = "2030300 This is test";
     char *ptr;
     long ret;
 
     ret = strtoul(str, &ptr, 10);
-    printf("The number (unsigned long integer) is &lu\n", ret);
+    printf("The number (unsigned long integer) is %lu\n", ret);
     printf("String part is |%s|", ptr);
     
     return (0);
