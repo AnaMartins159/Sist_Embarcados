3ce4eb417b1c4c69648ce527007d963e11301a30
leonardo-usp <leonardodvc@usp.br>
2022-06-16 10:28:45 -0300

    Add CaseStudy

---
+++ CaseStudy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char str[30] = "123456";
+    char *ptr;
+    long ret;
+
+    ret = strtoul(str, &ptr, 10);
+    printf("The number (unsigned long integer) is &lu\n", ret);
+    printf("String part is |%s|", ptr);
+    
+    return (0);
+}
